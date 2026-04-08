58e8e89c5ef132f99492d2de358c79ef1cca4d07
Abu Bakor Siddik (Shuvo) <141806136+Abs-shuvo@users.noreply.github.com>
2026-04-08 14:18:49 +0600

    created issue container card with all the specification with responsived & also fixed the filterable option for showing issues

--- script/home.js
+++ script/home.js
@@ -2,6 +2,15 @@ console.log("home.js script connecting");
 
 let allIssues = [];
 
+/* Badges */
+const getLabelColor = (label) => {
+    if (label === "bug") return "badge-error";
+    if (label === "help wanted") return "badge-warning";
+    if (label === "enhancement") return "badge-success";
+    if (label === "documentation") return "badge-info";
+    return "badge-neutral"; 
+};
+
 /* Fetching all Data */
 const loadIssues = () => {
     fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
@@ -22,14 +31,52 @@ const displayIssues = (issues) =>{
         const div = document.createElement('div');
 
         div.innerHTML=`
-        
-        `;
-    })
+            <div class="card bg-base-100 shadow p-5 mb-4 border-t-4 
+                ${issue.status === 'open' ? 'border-green-500' : 'border-purple-400'}">
+                <div class="flex justify-between">
+                    <span>${issue.status === 'open' ? '<i class="fa-solid fa-circle" style="color: rgb(99, 230, 190);"></i>' : '<i class="fa-solid fa-circle" style="color: rgb(177, 151, 252);"></i>'}</span>
+                    <span ${issue.priority==='high'?`class="bg-red-300 px-3 rounded-xl"`: issue.priority==='low' ? `class="bg-gray-400 px-3 rounded-xl"` : `class="bg-yellow-400 px-3 rounded-xl"`} >${issue.priority}</span>
+                </div>   
 
+                <div class="mt-2">
+                    <h2 class="font-semibold text-lg">${issue.title}</h2>
+                    <p class="text-gray-500 mt-2">${issue.description}</p>
+                </div>
+
+                <div class="flex flex-wrap gap-2 mt-2">
+                    ${issue.labels.map(label => `
+                        <span class="badge badge-soft ${getLabelColor(label)} ">
+                            ${label.toUpperCase()}
+                        </span>
+                    `).join('')}
+                </div>
+                <div class="divider"></div>
+                <div class=" flex flex-col space-y-2 text-sm text-gray-500">
+                    <span> by ${issue.author}</span>
+                    <span>${issue.createdAt}</span>
+                </div>
+            </div>
+        `;
+        issueContainer.appendChild(div);
+    })
 }
 
-const allIssuesShow=()=>{
 
+/* All Issue Show Button */
+const allIssuesShow=()=>{
+    displayIssues(allIssues);
+}
+
+/* Open issue Show Button */
+const openIssuesShow=()=>{
+    const openIssues = allIssues.filter(issue => issue.status === 'open')
+    displayIssues(openIssues);
+}
+
+/* Closed issue Show Button */
+const closedIssuesShow=()=>{
+    const closedIssues = allIssues.filter(issue => issue.status === 'closed')
+    displayIssues(closedIssues);
 }
 
 loadIssues();
